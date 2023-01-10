f40c0310228e9f0f033e0eb61ed185891972ec62
Dan Green <danngreen1@gmail.com>
2023-01-09 22:28:43 -0800

    rename block<=>buffer

--- src/audio_stream.hh
+++ src/audio_stream.hh
@@ -6,18 +6,19 @@
 #include "drivers/callable.hh"
 #include "drivers/codec_WM8731.hh"
 #include "drivers/i2c.hh"
+#include <functional>
 
-namespace LoppingDelay
+namespace LDKit
 {
 
 struct AudioStream {
 	// TODO: Verify this region is non-cacheable:
-	static inline AudioStreamConf::AudioInBlock audio_in_dma_block;
-	static inline AudioStreamConf::AudioOutBlock audio_out_dma_block;
+	static inline AudioStreamConf::AudioInBuffer audio_in_dma_buffer;
+	static inline AudioStreamConf::AudioOutBuffer audio_out_dma_buffer;
 
 public:
 	using AudioProcessFunction =
-		Function<void(const AudioStreamConf::AudioInBuffer &, AudioStreamConf::AudioOutBuffer &)>;
+		std::function<void(const AudioStreamConf::AudioInBlock &, AudioStreamConf::AudioOutBlock &)>;
 
 	AudioStream(AudioProcessFunction &&process_func)
 		: codec_i2c{codec_i2c_conf}
@@ -29,31 +30,24 @@ public:
 		HAL_RCCEx_PeriphCLKConfig(const_cast<RCC_PeriphCLKInitTypeDef *>(&sai_rcc_clk_conf));
 
 		codec.init();
-		codec.set_rx_buffer_start(audio_in_dma_block[0]);
-		codec.set_tx_buffer_start(audio_out_dma_block[0]);
-	}
+		codec.set_rx_buffer_start(audio_in_dma_buffer[0]);
+		codec.set_tx_buffer_start(audio_out_dma_buffer[0]);
 
-	void set_process_function(AudioProcessFunction &process) { _process_func = process; }
-
-	void start(AudioProcessFunction &process) {
-		_process_func = process;
 		codec.set_callbacks([this] { _process<1>(); }, [this] { _process<0>(); });
-		codec.start();
 	}
+
+	void start() { codec.start(); }
 
 	template<uint32_t buffer_half>
 	void _process() {
-		_process_func(audio_in_dma_block[buffer_half], audio_out_dma_block[buffer_half]);
+		_process_func(audio_in_dma_buffer[buffer_half], audio_out_dma_buffer[buffer_half]);
 	}
 
 private:
 	mdrivlib::I2CPeriph codec_i2c;
 	mdrivlib::CodecWM8731 codec;
 
-	AudioProcessFunction &_process_func;
-	float audio_load_smoothed = 0.f;
-	static constexpr float kSmoothCoef = 0.01; // 100 process blocks = 6400 samples: tau=0.133s
-	static constexpr float kinvSmoothCoef = 1.f - kSmoothCoef;
+	AudioProcessFunction _process_func;
 };
 
-} // namespace LoppingDelay
+} // namespace LDKit
