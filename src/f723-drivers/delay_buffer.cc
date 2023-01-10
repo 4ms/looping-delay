97b94ebaf1454633e94e8fe0e99fb777736caa9f
Dan Green <danngreen1@gmail.com>
2023-01-09 22:31:58 -0800

    Link region for SDRAM (TODO: remove if not used)

---
+++ src/f723-drivers/delay_buffer.cc
@@ -0,0 +1,11 @@
+#include "audio_buffer.hh"
+#include "conf/board_conf.hh"
+
+namespace LDKit
+{
+// DelayBufferT &get_audio_buffer() {
+// 	static __attribute__((section("SDRAM"))) DelayBufferT arr;
+// 	return arr;
+// }
+
+} // namespace LDKit
