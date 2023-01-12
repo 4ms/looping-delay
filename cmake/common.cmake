08b15c9eeae5a9447d3794747c3b2a8b60762b6a
Dan Green <danngreen1@gmail.com>
2023-01-11 17:31:38 -0800

    Set SDRAM pins and bank. WIP driver porting to F7

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -9,6 +9,7 @@ set(COMMON_SOURCES
     ${root}/lib/mdrivlib/drivers/i2c.cc
     ${root}/lib/mdrivlib/drivers/codec_WM8731.cc
     ${root}/lib/mdrivlib/drivers/hal_handlers.cc
+    ${root}/lib/mdrivlib/drivers/sdram.cc
     ${root}/lib/mdrivlib/target/stm32f7xx/drivers/interrupt_handler.cc
     ${root}/lib/mdrivlib/target/stm32f7xx/drivers/sai_tdm.cc
     ${root}/lib/libhwtests/src/AdcChecker.cc
--- src/system.hh
+++ src/system.hh
@@ -1,6 +1,8 @@
 #pragma once
 #include "conf/flash_layout.hh"
 #include "conf/rcc_conf.hh"
+#include "conf/sdram_conf.hh"
+#include "drivers/sdram.hh"
 #include "drivers/system.hh"
 
 namespace LDKit
@@ -11,6 +13,8 @@ struct System {
 		mdrivlib::System::SetVectorTable(AppStartAddr);
 		mdrivlib::SystemClocks::init_clocks(osc_conf, clk_conf, rcc_periph_conf);
 	}
+
+	mdrivlib::SDRAMPeriph sdram{SDRAM_AS4C_T7_conf};
 };
 
 } // namespace LDKit
