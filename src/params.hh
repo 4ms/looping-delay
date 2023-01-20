f7dd50a2d8ff9a4471b74b1fd1cd24151a233b11
Dan Green <danngreen1@gmail.com>
2023-01-19 21:31:18 -0800

    Calc time mult

--- src/params.hh
+++ src/params.hh
@@ -1,7 +1,7 @@
 #pragma once
 #include "audio_stream_conf.hh"
+#include "clock_mult_util.hh"
 #include "controls.hh"
-#include "exp_1voct_lut.hh"
 #include "util/countzip.hh"
 #include <cstdint>
 
@@ -26,7 +26,7 @@ struct ChannelMode {
 	InfState inf = InfState::Off;
 	bool reverse = false;
 	bool time_pot_quantized = true;
-	bool time_jack_quantized = true;
+	bool time_cv_quantized = true;
 	bool ping_locked = false;
 	bool quantize_mode_changes = true;
 	bool adjust_loop_end = false; // flag_pot_changed_revdown[TIME]
@@ -98,28 +98,24 @@ struct Params {
 			update_scroll_loop_amount();
 		} else {
 			float df = cv_state[DelayFeedCV].cur_val + pot_state[DelayFeedPot].cur_val;
-			delay_feed = std::clamp(df / 4095.f, 0.f, 4095.f);
+			delay_feed = std::clamp(df / 4095.f, 0.f, 1.f);
 
 			float fb = cv_state[FeedbackCV].cur_val + pot_state[FeedbackPot].cur_val;
-			feedback = std::clamp(fb / 4095.f, 0.f, 4095.f);
+			feedback = std::clamp(fb / 4095.f, 0.f, 1.f);
 		}
 
-		int16_t time_cv = 2048 - cv_state[TimeCV].cur_val;
+		int16_t time_cv = MathTools::plateau<60>(2048 - cv_state[TimeCV].cur_val);
+
 		int16_t time_pot = pot_state[TimePot].cur_val;
-		float time_mult;
-		if (modes.time_jack_quantized && modes.time_pot_quantized) {
-			time_mult = calc_clk_div_quantized(time_pot) * calc_clk_div_quantized(time_cv);
-		} else if (!modes.time_jack_quantized && !modes.time_pot_quantized) {
-			if (time_cv > 30) {
-				// Positive voltage on jack
-				float comp_cv = (float)time_cv * tracking_comp;
-				// TODO: interpolate lookup
-				time_mult = calc_clk_div_unquant(time_pot / exp_1voct[(unsigned)comp_cv]);
-			} else if (time_cv < -30) {
-				// negative boltage on jack
-			}
-			time_mult = calc_clk_div_quantized(time_pot) * calc_clk_div_quantized(time_cv);
-		}
+		float time_pot_mult =
+			modes.time_pot_quantized ? ClockMultUtil::calc_quantized(time_pot) : ClockMultUtil::calc_unquant(time_pot);
+
+		float time_cv_mult = modes.time_cv_quantized ? ClockMultUtil::calc_quantized(time_cv) :
+							 (time_cv < 0)			 ? ClockMultUtil::calc_unquant(time_cv) :
+													   ClockMultUtil::calc_voct(time_cv, tracking_comp);
+
+		auto time_switch = controls.read_time_switch();
+		float time_mult = adjust_time_by_switch(time_pot_mult * time_cv_mult, time_switch);
 
 		float mx = (controls.read_adc(MixCV) - 2048) + pot_state[MixPot].cur_val;
 		// TODO; use epp lut
@@ -151,6 +147,7 @@ struct Params {
 
 	void reset_loopled_tmr() {}
 
+private:
 	void update_pot_states() {
 		for (auto [i, pot] : enumerate(pot_state)) {
 			pot.cur_val = (int16_t)controls.read_adc(static_cast<AdcElement>(i++));
@@ -211,16 +208,16 @@ struct Params {
 		// Turning Time without INF down changes it back to Quant mode (default)
 		if (pot_state[TimePot].moved_while_inf_down) {
 			modes.time_pot_quantized = false;
-			modes.time_jack_quantized = false;
+			modes.time_cv_quantized = false;
 		} else if (pot_state[TimePot].moved) {
 			modes.time_pot_quantized = true;
 			if (!settings.auto_unquantize_timejack)
-				modes.time_jack_quantized = true;
+				modes.time_cv_quantized = true;
 		}
 
 		if (settings.auto_unquantize_timejack) {
 			// Auto Unquantize mode: set jack to Quant when time is > 47Hz (48000 / 1024 = 47Hz)
-			modes.time_jack_quantized = (divmult_time >= 1024);
+			modes.time_cv_quantized = (divmult_time >= 1024);
 		}
 	}
 
@@ -239,53 +236,8 @@ struct Params {
 		}
 	}
 
-	static constexpr float DivKnobValue[17] = {
-		1.f, 1.5f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f, 10.f, 11.f, 12.f, 13.f, 14.f, 15.f, 16.f};
-	static constexpr int16_t DivKnobDetents[18] = {
-		0, 40, 176, 471, 780, 1076, 1368, 1664, 1925, 2179, 2448, 2714, 2991, 3276, 3586, 3879, 4046, 4095};
-
-	static constexpr float calc_clk_div_quantized(int16_t adc_val) {
-		bool divide = false;
-		float val = DivKnobValue[16];
-
-		if (adc_val < 0) {
-			adc_val = -adc_val;
-			divide = true;
-		}
-		val = DivKnobValue[find_range_idx(adc_val)];
-		return divide ? (1.f / val) : val;
-	}
-
-	static constexpr float calc_clk_div_unquant(int16_t adc_val) {
-		bool divide = false;
-		if (adc_val < 0) {
-			adc_val = -adc_val;
-			divide = true;
-		}
-
-		float val;
-		unsigned range_i = find_range_idx(adc_val);
-		if (range_i == 0)
-			val = DivKnobValue[0];
-		else {
-			float floor = DivKnobValue[range_i - 1];
-			float ceil = DivKnobValue[range_i];
-			val = MathTools::map_value(adc_val, DivKnobDetents[range_i], DivKnobDetents[range_i + 1], floor, ceil);
-		}
-		return divide ? (1.f / val) : val;
-	}
-
-	static constexpr unsigned find_range_idx(uint16_t adc_val) {
-		static_assert(DivKnobDetents[0] == 0, "");
-		unsigned i;
-		for (i = 1; i < 17; i++) {
-			if (adc_val <= DivKnobDetents[i])
-				break;
-		}
-		return i - 1;
-	}
-
-	static constexpr float adjust_time_by_switch(float timeval, uint8_t switch_val) {
+	static constexpr float adjust_time_by_switch(float timeval, Controls::SwitchPos switch_pos) {
+		uint16_t switch_val = static_cast<uint16_t>(switch_pos);
 		if (switch_val == 0b10)
 			return timeval + 16.f; // switch up: 17-32
 		if (switch_val == 0b01)
@@ -319,40 +271,5 @@ private:
 };
 
 constexpr auto ParamsSize = sizeof(Params); // 164B
-
-static_assert(Params::calc_clk_div_quantized(0) == 1.0f, "");
-static_assert(Params::calc_clk_div_quantized(39) == 1.0f, "");
-static_assert(Params::calc_clk_div_quantized(40) == 1.0f, "");
-static_assert(Params::calc_clk_div_quantized(41) == 1.5f, "");
-static_assert(Params::calc_clk_div_quantized(4045) == 15.f, "");
-static_assert(Params::calc_clk_div_quantized(4046) == 15.f, "");
-static_assert(Params::calc_clk_div_quantized(4047) == 16.f, "");
-static_assert(Params::calc_clk_div_quantized(4095) == 16.f, "");
-
-static_assert(Params::find_range_idx(39) == 0, "");
-static_assert(Params::find_range_idx(40) == 0, "");
-static_assert(Params::find_range_idx(41) == 1, "");
-
-static_assert(Params::calc_clk_div_unquant(39) == 1.0f, "");
-static_assert(Params::calc_clk_div_unquant(40) == 1.000f, "");
-static_assert(Params::calc_clk_div_unquant(41) >= 1.003f, "");
-static_assert(Params::calc_clk_div_unquant(41) <= 1.004f, "");
-static_assert(Params::calc_clk_div_unquant(42) >= 1.007f, "");
-static_assert(Params::calc_clk_div_unquant(42) <= 1.008f, "");
-static_assert(Params::calc_clk_div_unquant(780) == 3.0f, "");
-static_assert(Params::calc_clk_div_unquant(928) == 3.5f, "");
-
-static_assert(Params::calc_clk_div_unquant(4046) == 15.000f, "");
-
-static_assert(Params::calc_clk_div_unquant(4047) >= 15.020f, "");
-static_assert(Params::calc_clk_div_unquant(4047) <= 15.021f, "");
-
-static_assert(Params::calc_clk_div_unquant(4048) >= 15.040f, "");
-static_assert(Params::calc_clk_div_unquant(4048) <= 15.041f, "");
-
-static_assert(Params::calc_clk_div_unquant(4094) >= 15.979f, "");
-static_assert(Params::calc_clk_div_unquant(4094) <= 15.980f, "");
-
-static_assert(Params::calc_clk_div_unquant(4095) == 16.f, "");
 
 } // namespace LDKit
