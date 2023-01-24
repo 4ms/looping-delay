f0187f185162d79d7f8b3d80e4a071092f1743c5
Dan Green <danngreen1@gmail.com>
2023-01-23 21:02:45 -0800

    add interface for disable_mode_changes

--- src/flags.hh
+++ src/flags.hh
@@ -9,6 +9,7 @@ private:
 	bool _time_changed = true;
 	bool _inf_changed = false;
 	bool _rev_changed = false;
+	bool _disable_mode_changes = false;
 	float _scroll_loop_amt = 0.f;
 
 public:
@@ -20,11 +21,15 @@ public:
 		return t;
 	}
 	bool take_inf_changed() {
+		if (_disable_mode_changes)
+			return false;
 		auto t = _inf_changed;
 		_inf_changed = false;
 		return t;
 	}
 	bool take_rev_changed() {
+		if (_disable_mode_changes)
+			return false;
 		auto t = _rev_changed;
 		_rev_changed = false;
 		return t;
@@ -38,6 +43,8 @@ public:
 	void set_time_changed() { _time_changed = true; }
 	void set_inf_changed() { _inf_changed = true; }
 	void set_rev_changed() { _rev_changed = true; }
+	void disable_mode_changes() { _disable_mode_changes = true; }
+	void enable_mode_changes() { _disable_mode_changes = false; }
 	void set_scroll_amt(float amt) { _scroll_loop_amt = amt; }
 	void add_scroll_amt(float amt) { _scroll_loop_amt += amt; }
 };
