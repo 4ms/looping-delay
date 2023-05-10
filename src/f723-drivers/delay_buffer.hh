8ce42bf7e15bb17aa69c16c99638f316667b871c
Dan Green <danngreen1@gmail.com>
2023-05-09 20:40:28 -0700

    comments

--- src/f723-drivers/delay_buffer.hh
+++ src/f723-drivers/delay_buffer.hh
@@ -9,6 +9,7 @@ namespace LDKit
 using DelayBuffer = std::array<int16_t, Brain::MemorySizeBytes / sizeof(int16_t)>;
 
 static inline DelayBuffer &get_delay_buffer() {
+	// Use placement new to construct an array in Brainboard's external memory
 	DelayBuffer::value_type *p = reinterpret_cast<DelayBuffer::value_type *>(Brain::MemoryStartAddr);
 	DelayBuffer *arr = new (p) DelayBuffer;
 	return (*arr);
