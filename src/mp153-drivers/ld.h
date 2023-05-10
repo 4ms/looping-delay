efaa018d30c8fcc689427536abbec0891f68c3f0
Dan Green <danngreen1@gmail.com>
2023-05-09 17:36:24 -0700

    Using #defines in ld.h to set memory layout (WIP)

--- src/mp153-drivers/ld.h
+++ src/mp153-drivers/ld.h
@@ -1,94 +1,22 @@
-#pragma once
-#include <stdint.h>
-// TODO: refactor this:
-//  #define A7_CODE 0xC2000040, etc in a header (with ONLY that)
-//  pre-process linker script to use these symbols
-//  no 'extern uint32_t LD_XXX[];' needed!
+/* Presume 256MB RAM */
 
-// Defined in linker scripts:
+#define A7_CODE 0xC2000040
+#define A7_CODE_SZ ((16 * 1024 * 1024) - 0x40) /*0x00FFFFD8*/
+#define A7_CODE_END 0xC3000000
 
-extern uint32_t LD_UNUSED0[];
-extern uint32_t LD_UNUSED0_SZ[];
-extern uint32_t LD_UNUSED0_END[];
+#define A7_RAM 0xC3000000
+#define A7_RAM_SZ (16 * 1024 * 1024) /*0x09000000*/
+#define A7_RAM_END 0xC4000000
 
-extern uint32_t LD_TTB[];
-extern uint32_t LD_TTB_SZ[];
-extern uint32_t LD_TTB_END[];
+#define A7_HEAP 0xC5000000
+#define A7_HEAP_SZ (175 * 1024 * 1024) /*0x0AF00000*/
+#define A7_HEAP_END 0xCFF00000
 
-// N
-extern uint32_t LD_A7_CODE[];
-extern uint32_t LD_A7_CODE_SZ[];
-extern uint32_t LD_A7_CODE_END[];
+#define DMABUF 0xCFF00000
+#define DMABUF_SZ (1 * 1024 * 1024) /*0x00100000*/
+#define DMABUF_END 0xD0000000
 
-// N-RW
-extern uint32_t LD_A7_RAM[];
-extern uint32_t LD_A7_RAM_SZ[];
-extern uint32_t LD_A7_RAM_END[];
-
-// N-RW
-extern uint32_t LD_A7_HEAP[];
-extern uint32_t LD_A7_HEAP_SZ[];
-extern uint32_t LD_A7_HEAP_END[];
-
-// SO
-extern uint32_t LD_DMABUF[];
-extern uint32_t LD_DMABUF_SZ[];
-extern uint32_t LD_DMABUF_END[];
-
-// SRAM1-3: Device-RW
-extern uint32_t LD_M4_CODE[];
-extern uint32_t LD_M4_CODE_SZ[];
-extern uint32_t LD_M4_CODE_END[];
-
-// SRAM4: Device-RW
-extern uint32_t LD_M4_STACK[];
-extern uint32_t LD_M4_STACK_SZ[];
-extern uint32_t LD_M4_STACK_END[];
-
-// SRAM4: Device-RW
-extern uint32_t LD_M4_SHARED_LIST[];
-extern uint32_t LD_A7_SHARED_LIST[];
-extern uint32_t LD_SHARED_LIST_SZ[];
-extern uint32_t LD_M4_SHARED_LIST_END[];
-
-// Not listed:
-// SYSRAM is SO
-
-//// as uint32_t's:
-
-// static uint32_t UNUSED0 = (uint32_t)LD_UNUSED0;
-// static uint32_t UNUSED0_SZ = (uint32_t)LD_UNUSED0_SZ;
-// static uint32_t UNUSED0_END = (uint32_t)LD_UNUSED0_END;
-
-// static uint32_t TTB = (uint32_t)LD_TTB;
-// static uint32_t TTB_SZ = (uint32_t)LD_TTB_SZ;
-// static uint32_t TTB_END = (uint32_t)LD_TTB_END;
-
-static uint32_t A7_CODE = (uint32_t)LD_A7_CODE;
-// static uint32_t A7_CODE_SZ = (uint32_t)LD_A7_CODE_SZ;
-static uint32_t A7_CODE_END = (uint32_t)LD_A7_CODE_END;
-
-static uint32_t A7_RAM = (uint32_t)LD_A7_RAM;
-static uint32_t A7_RAM_SZ = (uint32_t)LD_A7_RAM_SZ;
-// static uint32_t A7_RAM_END = (uint32_t)LD_A7_RAM_END;
-
-static uint32_t A7_HEAP = (uint32_t)LD_A7_HEAP;
-static uint32_t A7_HEAP_SZ = (uint32_t)LD_A7_HEAP_SZ;
-// static uint32_t A7_HEAP_END = (uint32_t)LD_A7_HEAP_END;
-
-static uint32_t DMABUF = (uint32_t)LD_DMABUF;
-static uint32_t DMABUF_SZ = (uint32_t)LD_DMABUF_SZ;
-// static uint32_t DMABUF_END = (uint32_t)LD_DMABUF_END;
-
-// static uint32_t M4_CODE = (uint32_t)LD_M4_CODE;
-// static uint32_t M4_CODE_SZ = (uint32_t)LD_M4_CODE_SZ;
-// static uint32_t M4_CODE_END = (uint32_t)LD_M4_CODE_END;
-
-// static uint32_t M4_STACK = (uint32_t)LD_M4_STACK;
-// static uint32_t M4_STACK_SZ = (uint32_t)LD_M4_STACK_SZ;
-// static uint32_t M4_STACK_END = (uint32_t)LD_M4_STACK_END;
-
-// static uint32_t M4_SHARED_LIST = (uint32_t)LD_M4_SHARED_LIST;
-// static uint32_t A7_SHARED_LIST = (uint32_t)LD_A7_SHARED_LIST;
-// static uint32_t SHARED_LIST_SZ = (uint32_t)LD_SHARED_LIST_SZ;
-// static uint32_t M4_SHARED_LIST_END = (uint32_t)LD_M4_SHARED_LIST_END;
+#define M4_SHARED_LIST 0x1005FF00
+#define A7_SHARED_LIST 0x3005FF00
+#define SHARED_LIST_SZ 256
+#define M4_SHARED_LIST_END 0x10060000
