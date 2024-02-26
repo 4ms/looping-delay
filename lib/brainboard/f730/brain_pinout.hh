cc38c7437a600de0fa9b2f164a8bae9b1f66c2c5
Dan Green <danngreen1@gmail.com>
2024-02-26 11:31:39 -0800

    git subrepo pull (merge) lib/brainboard
    subrepo:
      subdir:   "lib/brainboard"
      merged:   "627c676"
    upstream:
      origin:   "git@github.com:danngreen/brainboard.git"
      branch:   "main"
      commit:   "627c676"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "110b9eb"

--- lib/brainboard/f730/brain_pinout.hh
+++ lib/brainboard/f730/brain_pinout.hh
@@ -14,48 +14,48 @@ using PinAF = mdrivlib::PinAF;
 using AdcChanNum = mdrivlib::AdcChanNum;
 using TimChannelNum = mdrivlib::TimChannelNum;
 
-constexpr inline PinDef A0{GPIO::A, PinNum::_4};
-constexpr inline PinDef A1{GPIO::A, PinNum::_5};
-constexpr inline PinDef A2{GPIO::C, PinNum::_2};
-constexpr inline PinDef A3{GPIO::C, PinNum::_3};
-constexpr inline PinDef A4{GPIO::A, PinNum::_0};
-constexpr inline PinDef A5{GPIO::A, PinNum::_1};
-constexpr inline PinDef A6{GPIO::C, PinNum::_4};
-constexpr inline PinDef A7{GPIO::C, PinNum::_0};
+constexpr inline PinDef A0{GPIO::F, PinNum::_7};
+constexpr inline PinDef A1{GPIO::A, PinNum::_2};
+constexpr inline PinDef A2{GPIO::F, PinNum::_8};
+constexpr inline PinDef A3{GPIO::C, PinNum::_0};
+constexpr inline PinDef A4{GPIO::F, PinNum::_10};
+constexpr inline PinDef A5{GPIO::F, PinNum::_9};
+constexpr inline PinDef A6{GPIO::F, PinNum::_6};
+constexpr inline PinDef A7{GPIO::A, PinNum::_1};
 constexpr inline PinDef A8{GPIO::C, PinNum::_1};
-constexpr inline PinDef A9{GPIO::A, PinNum::_6};
+constexpr inline PinDef A9{GPIO::A, PinNum::_3};
 
-constexpr inline AdcChanNum A0AdcChan{AdcChanNum::_4};
-constexpr inline AdcChanNum A1AdcChan{AdcChanNum::_5};
-constexpr inline AdcChanNum A2AdcChan{AdcChanNum::_12};
-constexpr inline AdcChanNum A3AdcChan{AdcChanNum::_13};
-constexpr inline AdcChanNum A4AdcChan{AdcChanNum::_0};
-constexpr inline AdcChanNum A5AdcChan{AdcChanNum::_1};
-constexpr inline AdcChanNum A6AdcChan{AdcChanNum::_14};
-constexpr inline AdcChanNum A7AdcChan{AdcChanNum::_10};
+constexpr inline AdcChanNum A0AdcChan{AdcChanNum::_5};
+constexpr inline AdcChanNum A1AdcChan{AdcChanNum::_2};
+constexpr inline AdcChanNum A2AdcChan{AdcChanNum::_6};
+constexpr inline AdcChanNum A3AdcChan{AdcChanNum::_10};
+constexpr inline AdcChanNum A4AdcChan{AdcChanNum::_8};
+constexpr inline AdcChanNum A5AdcChan{AdcChanNum::_7};
+constexpr inline AdcChanNum A6AdcChan{AdcChanNum::_4};
+constexpr inline AdcChanNum A7AdcChan{AdcChanNum::_1};
 constexpr inline AdcChanNum A8AdcChan{AdcChanNum::_11};
-constexpr inline AdcChanNum A9AdcChan{AdcChanNum::_6};
+constexpr inline AdcChanNum A9AdcChan{AdcChanNum::_3};
 
-constexpr inline PinDef D0{GPIO::I, PinNum::_7};
-constexpr inline PinDef D1{GPIO::I, PinNum::_6};
-constexpr inline PinDef D2{GPIO::I, PinNum::_5};
-constexpr inline PinDef D3{GPIO::B, PinNum::_8};
-constexpr inline PinDef D4{GPIO::D, PinNum::_2};
+constexpr inline PinDef D0{GPIO::A, PinNum::_15};
+constexpr inline PinDef D1{GPIO::I, PinNum::_7};
+constexpr inline PinDef D2{GPIO::C, PinNum::_7};
+constexpr inline PinDef D3{GPIO::A, PinNum::_11};
+constexpr inline PinDef D4{GPIO::D, PinNum::_7};
 constexpr inline PinDef D5{GPIO::I, PinNum::_0};
-constexpr inline PinDef D6{GPIO::I, PinNum::_1};
-constexpr inline PinDef D7{GPIO::I, PinNum::_2};
-constexpr inline PinDef D8{GPIO::I, PinNum::_3};
-constexpr inline PinDef D9{GPIO::C, PinNum::_12};
-constexpr inline PinDef D10{GPIO::A, PinNum::_15};
-constexpr inline PinDef D11{GPIO::C, PinNum::_7};
-constexpr inline PinDef D12{GPIO::A, PinNum::_8};
-constexpr inline PinDef D13{GPIO::H, PinNum::_4};
-constexpr inline PinDef D14{GPIO::A, PinNum::_2};
-constexpr inline PinDef D15{GPIO::H, PinNum::_5};
-constexpr inline PinDef D16{GPIO::C, PinNum::_8};
-constexpr inline PinDef D17{GPIO::C, PinNum::_9};
-constexpr inline PinDef D18{GPIO::C, PinNum::_10};
-constexpr inline PinDef D19{GPIO::C, PinNum::_11};
+constexpr inline PinDef D6{GPIO::A, PinNum::_9};
+constexpr inline PinDef D7{GPIO::C, PinNum::_2};
+constexpr inline PinDef D8{GPIO::C, PinNum::_3};
+constexpr inline PinDef D9{GPIO::D, PinNum::_6};
+constexpr inline PinDef D10{GPIO::A, PinNum::_10};
+constexpr inline PinDef D11{GPIO::D, PinNum::_13};
+constexpr inline PinDef D12{GPIO::B, PinNum::_10};
+constexpr inline PinDef D13{GPIO::A, PinNum::_8};
+constexpr inline PinDef D14{GPIO::C, PinNum::_6};
+constexpr inline PinDef D15{GPIO::C, PinNum::_9};
+constexpr inline PinDef D16{GPIO::G, PinNum::_9};
+constexpr inline PinDef D17{GPIO::G, PinNum::_10};
+constexpr inline PinDef D18{GPIO::B, PinNum::_3};
+constexpr inline PinDef D19{GPIO::B, PinNum::_4};
 
 constexpr inline PinDef D16SdmmcDat0AF = {D16.gpio, D16.pin, PinAF::AltFunc12};
 constexpr inline PinDef D17SdmmcDat1AF = {D17.gpio, D17.pin, PinAF::AltFunc12};
@@ -141,12 +141,16 @@ constexpr inline mdrivlib::TimChanConf D17PwmConf{
 
 // D15-D19?
 
-constexpr inline PinDef Debug0{GPIO::H, PinNum::_13};
-constexpr inline PinDef Debug1{GPIO::H, PinNum::_14};
-constexpr inline PinDef Debug2{GPIO::H, PinNum::_15};
-constexpr inline PinDef Debug3{GPIO::D, PinNum::_3};
-constexpr inline PinDef ConsoleUartTX{GPIO::H, PinNum::_13, PinAF::AltFunc8};
-constexpr inline PinDef ConsoleUartRX{GPIO::H, PinNum::_14, PinAF::AltFunc8};
+// TODO: Debug0 and Debug1 are not on the schematic.. are there dummy pins or something along those lines?
+constexpr inline PinDef Debug0{GPIO::A, PinNum::_12};
+constexpr inline PinDef Debug1{GPIO::I, PinNum::_2};
+
+constexpr inline PinDef Debug2{GPIO::A, PinNum::_12};
+constexpr inline PinDef Debug3{GPIO::I, PinNum::_2};
+
+// TODO: make sure alt func is correct
+constexpr inline PinDef ConsoleUartTX{GPIO::C, PinNum::_10, PinAF::AltFunc8};
+constexpr inline PinDef ConsoleUartRX{GPIO::C, PinNum::_11, PinAF::AltFunc8};
 
 // TODO: do we put SAI, Codec I2C, Codec Reset here?
 
