178ffcdec977a330075fe1524301dbd015989533
Dan Green <danngreen1@gmail.com>
2024-03-07 10:27:41 -0800

    Pins for brainboard v1.1rc2

--- lib/brainboard/f746/brain_pinout.hh
+++ lib/brainboard/f746/brain_pinout.hh
@@ -3,8 +3,10 @@
 #include "drivers/pin.hh"
 #include "drivers/tim_pwm.hh"
 
-namespace Brain {
-namespace Pin {
+namespace Brain
+{
+namespace Pin
+{
 using PinDef = mdrivlib::PinDef;
 using GPIO = mdrivlib::GPIO;
 using PinNum = mdrivlib::PinNum;
@@ -12,24 +14,24 @@ using PinAF = mdrivlib::PinAF;
 using AdcChanNum = mdrivlib::AdcChanNum;
 using TimChannelNum = mdrivlib::TimChannelNum;
 
-constexpr inline PinDef A0{GPIO::F, PinNum::_7};
+constexpr inline PinDef A0{GPIO::A, PinNum::_4};
 constexpr inline PinDef A1{GPIO::A, PinNum::_2};
-constexpr inline PinDef A2{GPIO::F, PinNum::_8};
+constexpr inline PinDef A2{GPIO::A, PinNum::_5};
 constexpr inline PinDef A3{GPIO::C, PinNum::_0};
-constexpr inline PinDef A4{GPIO::F, PinNum::_10};
-constexpr inline PinDef A5{GPIO::F, PinNum::_9};
-constexpr inline PinDef A6{GPIO::F, PinNum::_6};
+constexpr inline PinDef A4{GPIO::A, PinNum::_7};
+constexpr inline PinDef A5{GPIO::A, PinNum::_6};
+constexpr inline PinDef A6{GPIO::A, PinNum::_0};
 constexpr inline PinDef A7{GPIO::A, PinNum::_1};
 constexpr inline PinDef A8{GPIO::C, PinNum::_1};
 constexpr inline PinDef A9{GPIO::A, PinNum::_3};
 
-constexpr inline AdcChanNum A0AdcChan{AdcChanNum::_5};
+constexpr inline AdcChanNum A0AdcChan{AdcChanNum::_4};
 constexpr inline AdcChanNum A1AdcChan{AdcChanNum::_2};
-constexpr inline AdcChanNum A2AdcChan{AdcChanNum::_6};
+constexpr inline AdcChanNum A2AdcChan{AdcChanNum::_5};
 constexpr inline AdcChanNum A3AdcChan{AdcChanNum::_10};
-constexpr inline AdcChanNum A4AdcChan{AdcChanNum::_8};
-constexpr inline AdcChanNum A5AdcChan{AdcChanNum::_7};
-constexpr inline AdcChanNum A6AdcChan{AdcChanNum::_4};
+constexpr inline AdcChanNum A4AdcChan{AdcChanNum::_7};
+constexpr inline AdcChanNum A5AdcChan{AdcChanNum::_6};
+constexpr inline AdcChanNum A6AdcChan{AdcChanNum::_0};
 constexpr inline AdcChanNum A7AdcChan{AdcChanNum::_1};
 constexpr inline AdcChanNum A8AdcChan{AdcChanNum::_11};
 constexpr inline AdcChanNum A9AdcChan{AdcChanNum::_3};
@@ -37,14 +39,14 @@ constexpr inline AdcChanNum A9AdcChan{AdcChanNum::_3};
 constexpr inline PinDef D0{GPIO::A, PinNum::_15};
 constexpr inline PinDef D1{GPIO::I, PinNum::_7};
 constexpr inline PinDef D2{GPIO::C, PinNum::_7};
-constexpr inline PinDef D3{GPIO::A, PinNum::_11};
+constexpr inline PinDef D3{GPIO::H, PinNum::_14};
 constexpr inline PinDef D4{GPIO::D, PinNum::_7};
 constexpr inline PinDef D5{GPIO::I, PinNum::_0};
-constexpr inline PinDef D6{GPIO::A, PinNum::_9};
+constexpr inline PinDef D6{GPIO::I, PinNum::_2};
 constexpr inline PinDef D7{GPIO::C, PinNum::_2};
 constexpr inline PinDef D8{GPIO::C, PinNum::_3};
 constexpr inline PinDef D9{GPIO::D, PinNum::_6};
-constexpr inline PinDef D10{GPIO::A, PinNum::_10};
+constexpr inline PinDef D10{GPIO::B, PinNum::_11};
 constexpr inline PinDef D11{GPIO::D, PinNum::_13};
 constexpr inline PinDef D12{GPIO::B, PinNum::_10};
 constexpr inline PinDef D13{GPIO::A, PinNum::_8};
@@ -55,6 +57,7 @@ constexpr inline PinDef D17{GPIO::G, PinNum::_10};
 constexpr inline PinDef D18{GPIO::B, PinNum::_3};
 constexpr inline PinDef D19{GPIO::B, PinNum::_4};
 
+// TODO: check these
 constexpr inline PinDef D16SdmmcDat0AF = {D16.gpio, D16.pin, PinAF::AltFunc12};
 constexpr inline PinDef D17SdmmcDat1AF = {D17.gpio, D17.pin, PinAF::AltFunc12};
 constexpr inline PinDef D18SdmmcDat2AF = {D18.gpio, D18.pin, PinAF::AltFunc12};
@@ -63,31 +66,31 @@ constexpr inline PinDef D9SdmmcClkAF = {D9.gpio, D9.pin, PinAF::AltFunc12};
 constexpr inline PinDef D4SdmmcCmdAF = {D4.gpio, D4.pin, PinAF::AltFunc12};
 
 constexpr inline mdrivlib::TimChanConf D0PwmConf{
-    .pin = {D0.gpio, D0.pin, PinAF::AltFunc1},
-    .TIM = TIM2_BASE,
-    .channum = TimChannelNum::_1,
+	.pin = {D0.gpio, D0.pin, PinAF::AltFunc1},
+	.TIM = TIM2_BASE,
+	.channum = TimChannelNum::_1,
 };
 
 constexpr inline mdrivlib::TimChanConf D1PwmConf{
-    .pin = {D1.gpio, D1.pin, PinAF::AltFunc3},
-    .TIM = TIM8_BASE,
-    .channum = TimChannelNum::_3,
+	.pin = {D1.gpio, D1.pin, PinAF::AltFunc3},
+	.TIM = TIM8_BASE,
+	.channum = TimChannelNum::_3,
 };
 
 // D2?
 
 constexpr inline mdrivlib::TimChanConf D3PwmConf{
-    .pin = {D3.gpio, D3.pin, PinAF::AltFunc1},
-    .TIM = TIM1_BASE,
-    .channum = TimChannelNum::_4,
+	.pin = {D3.gpio, D3.pin, PinAF::AltFunc1},
+	.TIM = TIM1_BASE,
+	.channum = TimChannelNum::_4,
 };
 
 // D4?
 
 constexpr inline mdrivlib::TimChanConf D5PwmConf{
-    .pin = {D5.gpio, D5.pin, PinAF::AltFunc2},
-    .TIM = TIM5_BASE,
-    .channum = TimChannelNum::_4,
+	.pin = {D5.gpio, D5.pin, PinAF::AltFunc2},
+	.TIM = TIM5_BASE,
+	.channum = TimChannelNum::_4,
 };
 
 // D6?
@@ -104,29 +107,29 @@ constexpr inline mdrivlib::TimChanConf D5PwmConf{
 // D9
 
 constexpr inline mdrivlib::TimChanConf D10PwmConf{
-    .pin = {D10.gpio, D10.pin, PinAF::AltFunc1},
-    .TIM = TIM1_BASE,
-    .channum = TimChannelNum::_3,
+	.pin = {D10.gpio, D10.pin, PinAF::AltFunc1},
+	.TIM = TIM1_BASE,
+	.channum = TimChannelNum::_3,
 };
 
 constexpr inline mdrivlib::TimChanConf D11PwmConf{
-    .pin = {D11.gpio, D11.pin, PinAF::AltFunc2},
-    .TIM = TIM4_BASE,
-    .channum = TimChannelNum::_2,
+	.pin = {D11.gpio, D11.pin, PinAF::AltFunc2},
+	.TIM = TIM4_BASE,
+	.channum = TimChannelNum::_2,
 };
 
 // D12
 constexpr inline mdrivlib::TimChanConf D12PwmConf{
-    .pin = {D12.gpio, D12.pin, PinAF::AltFunc1},
-    .TIM = TIM2_BASE,
-    .channum = TimChannelNum::_3,
+	.pin = {D12.gpio, D12.pin, PinAF::AltFunc1},
+	.TIM = TIM2_BASE,
+	.channum = TimChannelNum::_3,
 };
 // D13
 
 constexpr inline mdrivlib::TimChanConf D14PwmConf{
-    .pin = {D14.gpio, D14.pin, PinAF::AltFunc2},
-    .TIM = TIM3_BASE,
-    .channum = TimChannelNum::_1,
+	.pin = {D14.gpio, D14.pin, PinAF::AltFunc2},
+	.TIM = TIM3_BASE,
+	.channum = TimChannelNum::_1,
 };
 
 // D16	TIM3_CH3 (AF2)
@@ -149,11 +152,11 @@ constexpr inline mdrivlib::TimChanConf D14PwmConf{
 
 // TODO: Debug0 and Debug1 are not on the schematic.. are there dummy pins or
 // something along those lines?
-constexpr inline PinDef Debug0{GPIO::A, PinNum::_12};
-constexpr inline PinDef Debug1{GPIO::I, PinNum::_2};
+constexpr inline PinDef Debug0{GPIO::B, PinNum::_15};
+constexpr inline PinDef Debug1{GPIO::B, PinNum::_14};
 
-constexpr inline PinDef Debug2{GPIO::A, PinNum::_12};
-constexpr inline PinDef Debug3{GPIO::I, PinNum::_2};
+constexpr inline PinDef Debug2{GPIO::B, PinNum::_15};
+constexpr inline PinDef Debug3{GPIO::B, PinNum::_14};
 
 // TODO: make sure alt func is correct
 constexpr inline PinDef ConsoleUartTX{GPIO::C, PinNum::_10, PinAF::AltFunc8};
