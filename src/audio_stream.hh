be5d7e962d0811328403eebd9d5f8acb633d5ec6
Dan Green <danngreen1@gmail.com>
2024-03-08 10:26:30 -0800

    Update audio stream to use Brain codec conf

--- src/audio_stream.hh
+++ src/audio_stream.hh
@@ -1,10 +1,7 @@
 #pragma once
 
 #include "audio_stream_conf.hh"
-#include "conf/codec_conf.hh"
-#include "conf/rcc_conf.hh"
-#include "debug.hh"
-#include "drivers/callable.hh"
+#include "codec_conf.hh"
 #include "drivers/codec_PCM3060.hh"
 #include "drivers/i2c.hh"
 #include <functional>
@@ -24,18 +21,24 @@ public:
 		std::function<void(const AudioStreamConf::AudioInBlock &, AudioStreamConf::AudioOutBlock &)>;
 
 	AudioStream(AudioProcessFunction &&process_func)
-		: codec_i2c{Board::codec_i2c_conf}
-		, codec{codec_i2c, Board::sai_conf}
+		: codec_i2c{Brain::codec_i2c_conf}
+		, codec{codec_i2c, Brain::sai_conf<48000, 24>}
 		, _process_func{std::move(process_func)} {
 
-		codec.init(mdrivlib::CodecPCM3060Register::default_setup_i2s_24bit_hpf);
+		auto init_ok = codec.init(mdrivlib::CodecPCM3060Register::default_setup_i2s_24bit_hpf);
+		if (init_ok != mdrivlib::CodecPCM3060::Error::CODEC_NO_ERR)
+			; // __BKPT(); // TODO: display error, flash lights
 		codec.set_rx_buffer_start(audio_in_dma_buffer);
 		codec.set_tx_buffer_start(audio_out_dma_buffer);
 		codec.set_callbacks([this] { _process<1>(); }, [this] { _process<0>(); });
 	}
 
-	void start() { codec.start(); }
-	void stop() { codec.stop(); }
+	void start() {
+		codec.start();
+	}
+	void stop() {
+		codec.stop();
+	}
 
 	void set_callback(AudioProcessFunction &&process_func) {
 		stop();
