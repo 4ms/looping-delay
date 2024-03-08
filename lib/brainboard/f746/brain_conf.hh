4b4814fdf3a7b4558d6548c9ea9a607ca977953c
Dan Green <danngreen1@gmail.com>
2024-03-08 11:19:48 -0800

    Fix f746 brainboard ADC conf

--- lib/brainboard/f746/brain_conf.hh
+++ lib/brainboard/f746/brain_conf.hh
@@ -17,7 +17,7 @@ constexpr inline auto AdcSampTime = mdrivlib::AdcSamplingTime::_28Cycles;
 
 struct PotAdcConf : mdrivlib::DefaultAdcPeriphConf {
 	static constexpr auto resolution = mdrivlib::AdcResolution::Bits12;
-	static constexpr auto adc_periph_num = mdrivlib::AdcPeriphNum::_3;
+	static constexpr auto adc_periph_num = mdrivlib::AdcPeriphNum::_2;
 	static constexpr auto oversample = DebugDisableAdcIRQ ? true : false;
 	static constexpr auto clock_div = mdrivlib::AdcClockSourceDiv::APBClk_Div4;
 
@@ -26,10 +26,10 @@ struct PotAdcConf : mdrivlib::DefaultAdcPeriphConf {
 
 	struct DmaConf : mdrivlib::DefaultAdcPeriphConf::DmaConf {
 		static constexpr auto DMAx = 2;
-		static constexpr auto StreamNum = 0;
-		static constexpr auto RequestNum = DMA_CHANNEL_2;
+		static constexpr auto StreamNum = 2;
+		static constexpr auto RequestNum = DMA_CHANNEL_1;
 		static constexpr auto dma_priority = Low;
-		static constexpr IRQn_Type IRQn = DMA2_Stream0_IRQn;
+		static constexpr IRQn_Type IRQn = DMA2_Stream2_IRQn;
 		static constexpr uint32_t pri = 0;
 		static constexpr uint32_t subpri = 0;
 	};
@@ -48,10 +48,10 @@ struct CVAdcConf : mdrivlib::DefaultAdcPeriphConf {
 
 	struct DmaConf : mdrivlib::DefaultAdcPeriphConf::DmaConf {
 		static constexpr auto DMAx = 2;
-		static constexpr auto StreamNum = 4;
+		static constexpr auto StreamNum = 0;
 		static constexpr auto RequestNum = DMA_CHANNEL_0;
 		static constexpr auto dma_priority = Low;
-		static constexpr IRQn_Type IRQn = DMA2_Stream4_IRQn;
+		static constexpr IRQn_Type IRQn = DMA2_Stream0_IRQn;
 		static constexpr uint32_t pri = 0;
 		static constexpr uint32_t subpri = 0;
 	};
