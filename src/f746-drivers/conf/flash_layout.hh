f0dc7a77cb34ecef6f802b65d5cc4fd43dccb9d6
Dan Green <danngreen1@gmail.com>
2024-07-12 13:51:19 -0700

    Alter flash layout for bootloader

--- src/f746-drivers/conf/flash_layout.hh
+++ src/f746-drivers/conf/flash_layout.hh
@@ -1,7 +1,7 @@
 #pragma once
 
-#define APP_FLASH_ADDR 0x08000000
-#define APP_START_ADDR 0x08000000
+#define APP_FLASH_ADDR 0x08010000
+#define APP_START_ADDR 0x08010000
 
 #ifdef __cplusplus
 // TODO: put APP_FLASH_ADDR etc in a .h file, which is used to preprocess linker script
@@ -10,13 +10,13 @@
 
 #include "flash_sectors.hh"
 
-// constexpr inline uint32_t BootloaderFlashAddr = get_sector_addr(0);
-constexpr inline uint32_t SettingsFlashAddr = get_sector_addr(5);
-constexpr inline uint32_t AppFlashAddr = get_sector_addr(0);
-// constexpr inline uint32_t BootloaderReceiveAddr = get_sector_addr(6);
+constexpr inline uint32_t BootloaderFlashAddr = get_sector_addr(0);	  // 32k Bootloader
+constexpr inline uint32_t SettingsFlashAddr = get_sector_addr(1);	  // 32k Settings
+constexpr inline uint32_t AppFlashAddr = get_sector_addr(2);		  // 192k+256k for app
+constexpr inline uint32_t BootloaderReceiveAddr = get_sector_addr(2); // receive at same address
 
 constexpr inline uint32_t AppStartAddr = AppFlashAddr;
-// constexpr inline uint32_t BootloaderStartAddr = BootloaderFlashAddr;
+constexpr inline uint32_t BootloaderStartAddr = BootloaderFlashAddr;
 
 static_assert(APP_FLASH_ADDR == AppFlashAddr);
 static_assert(APP_START_ADDR == AppStartAddr);
